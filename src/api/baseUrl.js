3e1361c01246ae3fc9777e47fa24e75b815cfb56
yq <1392227049@qq.com>
2018-12-21 09:15:05 +0800

    add api

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,9 +1,14 @@
 import { fetch } from './http'
 import { ipurl } from './env'
 
-const login = (data)=>{
-    return fetch('post',ipurl+'/public/?s=admin/Login/login',data);
+
+class api  {
+    login(data) {
+        return fetch('post',`${ipurl}/public/?s=admin/Login/login`,data)
+    }
 }
-export {
-    login
-}
+
+
+
+export default new api
+
